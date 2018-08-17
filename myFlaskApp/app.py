3e910e818a8a9e436feebab29083d9926fdc6cee
ariefjalil <up.prozac3@gmail.com>
2018-08-17 19:53:06 +0800

    app.config MYSQL

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'myflaskapp'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursoR'
 
 #initialize MYSQL
 mysql = MySQL(app)
