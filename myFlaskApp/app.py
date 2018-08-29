9485bbf87cf642ed462f6f2a1580cb3e46077aa7
ariefjalil <up.prozac3@gmail.com>
2018-08-29 13:25:43 +0800

    database

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '123456'
-app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_DB'] = 'flaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 #initialize MYSQL
