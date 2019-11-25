28e0a508dafd0f3f5ad8d090c2bdc5c02847f8fa
Arief Jalil <30983999+ariefjalil@users.noreply.github.com>
2019-11-25 17:33:43 +0800

    Update app.py

--- myFlaskApp/app.py
+++ myFlaskApp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging #call templates
 from flaskext.mysql import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt  #to encrypt Password
+from passlib.hash import sha256_crypt  #Encrypt password
 
 
 
